3498cbaabafdf2debfceb5c2fc584cd3ff317a53
Matheus Henrique <matheus_henriquealves@hotmail.com>
2020-09-16 12:14:10 -0300

    Melhorias

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,239 +3,285 @@ import { AlertController } from "@ionic/angular";
 import { ToastController } from "@ionic/angular";
 import { DadosCalculoService } from "../dados-calculo.service";
 @Component({
-  selector: "app-tab1",
-  templateUrl: "tab1.page.html",
-  styleUrls: ["tab1.page.scss"],
+	selector: "app-tab1",
+	templateUrl: "tab1.page.html",
+	styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-  public peca: any = {
-    peca: "Peça",
-    desc: "",
-    lmin: null,
-    lmax: null,
-    obtido: [],
-  };
-  public result: any = { resultado: false, dados: {} };
+	public peca: any = {
+		peca: "Peça",
+		desc: "",
+		lmin: null,
+		lmax: null,
+		obtido: [],
+	};
+	public resultado: any = { resultado: false, dados: {} };
 
-  constructor(
-    private alertController: AlertController,
-    private toast: ToastController,
-    private serviceDados: DadosCalculoService
-  ) {}
+	constructor(
+		private alertController: AlertController,
+		private toast: ToastController,
+		private serviceDados: DadosCalculoService
+	) { }
 
-  async addObtido() {
-    let alert = await this.alertController.create({
-      header: "Valor Obtido",
-      inputs: [
-        {
-          name: "valor",
-          type: "number",
-          placeholder: "Digite o valor obtido",
-        },
-      ],
-      buttons: [
-        {
-          text: "Cancelar",
-          role: "cancel",
-          cssClass: "secondary",
-        },
-        {
-          text: "Adicionar",
-          handler: (value) => {
-            this.peca.obtido.push({ valor: parseFloat(value.valor) });
-          },
-        },
-      ],
-    });
+	async addObtido() {
+		let alert = await this.alertController.create({
+			header: "Valor Obtido",
+			inputs: [
+				{
+					name: "valor",
+					type: "number",
+					placeholder: "Digite o valor obtido",
+				},
+			],
+			buttons: [
+				{
+					text: "Cancelar",
+					role: "cancel",
+					cssClass: "secondary",
+				},
+				{
+					text: "Adicionar",
+					handler: (value) => {
+						this.peca.obtido.push({ valor: parseFloat(value.valor) });
+					},
+				},
+			],
+		});
 
-    await alert.present();
-  }
+		await alert.present();
+	}
 
-  async alteraPeca() {
-    let alert = await this.alertController.create({
-      header: "Informar Peça",
-      inputs: [
-        {
-          name: "peca",
-          type: "text",
-          placeholder: "Digite a identificação da peça",
-        },
-      ],
-      buttons: [
-        {
-          text: "Cancelar",
-          role: "cancel",
-          cssClass: "secondary",
-        },
-        {
-          text: "Salvar",
-          handler: (value) => {
-            this.peca.peca = value.peca;
-          },
-        },
-      ],
-    });
+	async alteraPeca() {
+		let alert = await this.alertController.create({
+			header: "Informar Peça",
+			inputs: [
+				{
+					name: "peca",
+					type: "text",
+					placeholder: "Digite a identificação da peça",
+				},
+			],
+			buttons: [
+				{
+					text: "Cancelar",
+					role: "cancel",
+					cssClass: "secondary",
+				},
+				{
+					text: "Salvar",
+					handler: (value) => {
+						this.peca.peca = value.peca;
+					},
+				},
+			],
+		});
 
-    await alert.present();
-  }
+		await alert.present();
+	}
 
-  removeObtido(index) {
-    this.peca.obtido.splice(index, 1);
-  }
+	removeObtido(index) {
+		this.peca.obtido.splice(index, 1);
+	}
 
-  async validaDados() {
-    if (this.peca.lmin == null && this.peca.lmax == null) {
-      let error = await this.toast.create({
-        message:
-          "Por favor informe pelo menos um dos limites. (limite inferior ou limite superior)",
-        duration: 2000,
-        color: "danger",
-      });
+	async validaDados() {
+		if (this.peca.lmin == null && this.peca.lmax == null) {
+			let error = await this.toast.create({
+				message:
+					"Por favor informe pelo menos um dos limites. (limite inferior ou limite superior)",
+				duration: 2000,
+				color: "danger",
+			});
 
-      error.present();
-      return false;
-    } else if (this.peca.obtido.length <= 2) {
-      let error = await this.toast.create({
-        message:
-          "Por favor informe pelo menos 3 valores para continuar com o calculo",
-        duration: 2000,
-        color: "danger",
-      });
+			error.present();
+			return false;
+		} else if (this.peca.obtido.length <= 2) {
+			let error = await this.toast.create({
+				message:
+					"Por favor informe pelo menos 3 valores para continuar com o calculo",
+				duration: 2000,
+				color: "danger",
+			});
 
-      error.present();
-      return false;
-    }
+			error.present();
+			return false;
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  async calcula() {
-    if (await this.validaDados()) {
-      let obtidos = this.converteObtidos(this.peca.obtido);
-      let variancia = this.calculaVariancia(this.peca.obtido);
+	async calcula() {
+		if (await this.validaDados()) {
+			let obtidos = this.converteObtidos(this.peca.obtido);
+			let variancia = this.calculaVariancia(this.peca.obtido);
 
-      let mediaObtidos = this.calculaMedia(obtidos);
-      let mediaVaria = this.calculaMedia(variancia);
-      let desvio = this.calculaDesvioPadrao(obtidos);
+			let mediaObtidos = this.calculaMedia(obtidos);
+			let mediaVaria = this.calculaMedia(variancia);
+			let desvio = this.calculaDesvioPadrao(obtidos);
 
-      let ppmin = null;
-      if (this.peca.lmin != null) {
-        ppmin = this.calculaPPMin(this.peca.lmin, mediaObtidos, desvio);
-      }
+			let cartaX = { lsc: this.calculaXLSC(mediaObtidos, mediaVaria), lic: this.calculaXLIC(mediaObtidos, mediaVaria) };
+			let cartaAM = { lsc: this.calculaAMLSC(mediaVaria), lic: 0 };
 
-      let ppmax = null;
-      if (this.peca.lmax != null) {
-        ppmax = this.calculaPPMax(this.peca.lmax, mediaObtidos, desvio);
-      }
+			let ppmin = null;
+			if (this.peca.lmin != null) {
+				ppmin = this.calculaPPMin(this.peca.lmin, mediaObtidos, desvio);
+			}
 
-      let pp = null;
-      if (ppmax != null && ppmin != null) {
-        pp = this.calculaPP(this.peca.lmax, this.peca.lmin, desvio);
-      }
+			let ppmax = null;
+			if (this.peca.lmax != null) {
+				ppmax = this.calculaPPMax(this.peca.lmax, mediaObtidos, desvio);
+			}
 
-      let ppk = this.calculaPPK(ppmin, ppmax);
+			let pp = null;
+			if (ppmax != null && ppmin != null) {
+				pp = this.calculaPP(this.peca.lmax, this.peca.lmin, desvio);
+			}
 
-      this.result = {
-        resultado: true,
-        dados: {
-          mediaObtidos: mediaObtidos.toFixed(2),
-          desvio: desvio.toFixed(2),
-          mediaVariancia: mediaVaria.toFixed(2),
-          pp: pp != null ? pp.toFixed(2) : "LNC",
-          ppmin: ppmin != null ? ppmin.toFixed(2) : "LNC",
-          ppmax: ppmax != null ? ppmax.toFixed(2) : "LNC",
-          ppk: ppk.toFixed(2),
-          lmin: this.peca.lmin.toFixed(2),
-          lmax: this.peca.lmax.toFixed(2),
-        },
-      };
+			let ppk = this.calculaPPK(ppmin, ppmax);
 
-      let fim = await this.toast.create({
-        message: "Calculado com Sucesso!",
-        duration: 2000,
-        color: "success",
-      });
+			this.resultado = {
+				resultado: true,
+				dados: {
+					limites: {
+						lmin: this.peca.lmin != null ? this.peca.lmin.toFixed(2) : "*",
+						lmax: this.peca.lmax != null ? this.peca.lmax.toFixed(2) : "*"
+					},
 
-      fim.present();
+					resultado: {
+						pp: pp != null ? pp.toFixed(2) : "*",
+						ppmin: ppmin != null ? ppmin.toFixed(2) : "*",
+						ppmax: ppmax != null ? ppmax.toFixed(2) : "*",
+						ppk: ppk.toFixed(2)
+					},
 
-      this.serviceDados.setDados({
-        calculo: true,
-        dados: obtidos,
-        result: this.result,
-      });
-    }
-  }
+					dados: {
+						mediaObtidos: mediaObtidos.toFixed(2),
+						desvio: desvio.toFixed(2),
+						mediaVariancia: mediaVaria.toFixed(2)
+					},
 
-  calculaVariancia(arrayOb) {
-    let newArr = [];
+					valoreVariancia: {
+						variancia
+					},
 
-    let ant = 0;
-    let primeiro = true;
+					cartax: {
+						lsc: cartaX.lsc != null ? cartaX.lsc.toFixed(2) : "*",
+						lm: mediaObtidos.toFixed(2),
+						lic: cartaX.lic != null ? cartaX.lic.toFixed(2) : "*"
+					},
 
-    arrayOb.forEach((element) => {
-      if (!primeiro) {
-        let diff = ant - element.valor;
+					cartaam: {
+						lsc: cartaAM.lsc != null ? cartaAM.lsc.toFixed(2) : "*",
+						lm: mediaVaria.toFixed(2),
+						lic: cartaAM.lic != null ? cartaAM.lic.toFixed(2) : "*"
+					}
+				},
+			};
 
-        diff = Math.abs(diff);
+			let fim = await this.toast.create({
+				message: "Calculado com Sucesso!",
+				duration: 2000,
+				color: "success",
+			});
 
-        newArr.push(diff);
-      }
-      primeiro = false;
-      ant = element.valor;
-    });
+			fim.present();
 
-    return newArr;
-  }
+			this.serviceDados.setDados({
+				detalhes : this.peca,
+				calculo: true,
+				obtidos: obtidos,
+				resultados: this.resultado,
+			});
+		}
+	}
 
-  converteObtidos(arrayOb) {
-    let newArr = [];
-    arrayOb.forEach((element) => {
-      newArr.push(element.valor);
-    });
-    return newArr;
-  }
+	calculaVariancia(arrayOb) {
+		let newArr = [];
 
-  calculaMedia(arrOb) {
-    let calculo = arrOb.reduce((a, b) => a + b) / arrOb.length;
-    return Math.abs(calculo);
-  }
+		let ant = 0;
+		let primeiro = true;
 
-  calculaDesvioPadrao(arrOb) {
-    let media = arrOb.reduce((total, valor) => total + valor / arrOb.length, 0);
+		arrayOb.forEach((element) => {
+			if (!primeiro) {
+				let diff = ant - element.valor;
 
-    let variancia = arrOb.reduce(
-      (total, valor) => total + Math.pow(media - valor, 2) / (arrOb.length - 1),
-      0
-    );
-    return Math.sqrt(variancia);
-  }
+				diff = Math.abs(diff);
 
-  calculaPP(lmax, lmin, desvio) {
-    let calculo = (lmax - lmin) / (6 * desvio);
-    return Math.abs(calculo);
-  }
+				newArr.push(diff);
+			}
+			primeiro = false;
+			ant = element.valor;
+		});
 
-  calculaPPMin(lmax, media, desvio) {
-    let calculo = (lmax - media) / (3 * desvio);
-    return Math.abs(calculo);
-  }
+		return newArr;
+	}
 
-  calculaPPMax(lmin, media, desvio) {
-    let calculo = (lmin - media) / (3 * desvio);
-    return Math.abs(calculo);
-  }
+	converteObtidos(arrayOb) {
+		let newArr = [];
+		arrayOb.forEach((element) => {
+			newArr.push(element.valor);
+		});
+		return newArr;
+	}
 
-  calculaPPK(lmin, lmax) {
-    if (lmin == null) {
-      return lmax;
-    } else if (lmax == null) {
-      return lmin;
-    } else {
-      if (lmin < lmax) {
-        return lmin;
-      } else {
-        return lmax;
-      }
-    }
-  }
+	calculaMedia(arrOb) {
+		let calculo = arrOb.reduce((a, b) => a + b) / arrOb.length;
+		return Math.abs(calculo);
+	}
+
+	calculaDesvioPadrao(arrOb) {
+		let media = arrOb.reduce((total, valor) => total + valor / arrOb.length, 0);
+
+		let variancia = arrOb.reduce(
+			(total, valor) => total + Math.pow(media - valor, 2) / (arrOb.length - 1), 0
+		);
+		return Math.sqrt(variancia);
+	}
+
+	calculaPP(lmax, lmin, desvio) {
+		let calculo = (lmax - lmin) / (6 * desvio);
+		return Math.abs(calculo);
+	}
+
+	calculaPPMin(lmax, media, desvio) {
+		let calculo = (lmax - media) / (3 * desvio);
+		return Math.abs(calculo);
+	}
+
+	calculaPPMax(lmin, media, desvio) {
+		let calculo = (lmin - media) / (3 * desvio);
+		return Math.abs(calculo);
+	}
+
+	calculaPPK(lmin, lmax) {
+		if (lmin == null) {
+			return lmax;
+		} else if (lmax == null) {
+			return lmin;
+		} else {
+			if (lmin < lmax) {
+				return lmin;
+			} else {
+				return lmax;
+			}
+		}
+	}
+
+	/**
+	 * Calculo das Cartas
+	 */
+	//Limite superior da Carta X
+	calculaXLSC(lm: number, desvio: number) {
+		return lm + 3 * (desvio / 1.128);
+	}
+
+	//Limite inferior da Carta X
+	calculaXLIC(lm: number, desvio: number) {
+		return lm - 3 * (desvio / 1.128);
+	}
+
+	//Limite superior da Carta AM
+	calculaAMLSC(desvio: number) {
+		return 3.267 * desvio;
+	}
+
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,63 +6,104 @@ import { ScreenOrientation } from "@ionic-native/screen-orientation/ngx";
 
 import { Platform } from "@ionic/angular";
 @Component({
-  selector: "app-tab2",
-  templateUrl: "tab2.page.html",
-  styleUrls: ["tab2.page.scss"],
+	selector: "app-tab2",
+	templateUrl: "tab2.page.html",
+	styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
-  public chartData: ChartDataSets[] = [
-    { data: [], label: "Valores Obtidos" },
-    { data: [], label: "Limite Inferior" },
-    { data: [], label: "Limite Superior" },
-  ];
-  public chartType: ChartType = "line";
-  public chartLabels: Label[];
+	public cartaX: ChartDataSets[] = [
+		{ data: [], label: "Valores Obtidos", fill: false, lineTension: 0, radius: 5, borderColor: 'rgba(21,101,192 ,1)', pointBackgroundColor: 'rgba(13,71,161 ,1)' },
+		{ data: [], label: "LSC", fill: false, radius: 0, borderColor: 'rgba(198,40,40 ,1)', pointBackgroundColor: 'rgba(183,28,28 ,1)' },
+		{ data: [], label: "LM", fill: false, radius: 0, borderColor: 'rgba(85,139,47 ,1)', pointBackgroundColor: 'rgba(51,105,30 ,1)' },
+		{ data: [], label: "LIC", fill: false, radius: 0, borderColor: 'rgba(198,40,40 ,1)', pointBackgroundColor: 'rgba(183,28,28 ,1)' }
+	];
 
-  public sizes = { height : '0px'}
 
-  public dadosCalculo: any = { calculo: false, dados: [] };
+	public cartaAM: ChartDataSets[] = [
+		{ data: [], label: "Valores Obtidos", fill: false, lineTension: 0, radius: 5, borderColor: 'rgba(21,101,192 ,1)', pointBackgroundColor: 'rgba(13,71,161 ,1)' },
+		{ data: [], label: "LSC", fill: false, radius: 0, borderColor: 'rgba(198,40,40 ,1)', pointBackgroundColor: 'rgba(183,28,28 ,1)' },
+		{ data: [], label: "LM", fill: false, radius: 0, borderColor: 'rgba(85,139,47 ,1)', pointBackgroundColor: 'rgba(51,105,30 ,1)' },
+		{ data: [], label: "LIC", fill: false, radius: 0, borderColor: 'rgba(198,40,40 ,1)', pointBackgroundColor: 'rgba(183,28,28 ,1)' }
+	];
 
-  constructor(
-    private serviceDados: DadosCalculoService,
-    private screenOrientation: ScreenOrientation,
-    public platform: Platform
-  ) {
-    this.setHeight();
-  }
 
-  ionViewWillEnter() {
-    console.log(window.innerHeight);
-    if (this.platform.is("mobile")) {
-      this.screenOrientation.lock(
-        this.screenOrientation.ORIENTATIONS.LANDSCAPE
-      );
-    }
-    this.dadosCalculo = this.serviceDados.getDados();
-    this.preencheGraph();
-  }
+	public chartType: ChartType = "line";
+	public labelsAM: Label[];
+	public labelsX: Label[];
 
-  setHeight(){
-    this.sizes.height = (window.innerHeight - 80) + 'px';
-  }
+	public dadosCalculo: any = { calculo: false, dados: [] };
 
-  ionViewDidLeave() {
-    if (this.platform.is("mobile")) {
-      this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-    }
-  }
+	constructor(
+		private serviceDados: DadosCalculoService,
+		private screenOrientation: ScreenOrientation,
+		public platform: Platform
+	) { }
 
-  preencheGraph() {
-    this.chartLabels = [];
-    this.chartData[0].data = [];
-    if (this.dadosCalculo.dados?.length > 0) {
-      for (let index = 0; index < this.dadosCalculo.dados.length; index++) {
-        const element = this.dadosCalculo.dados[index];
-        this.chartLabels.push(`Medição ${index + 1}`);
-        this.chartData[0].data.push(element);
-        this.chartData[1].data.push(this.dadosCalculo.result.dados.lmin);
-        this.chartData[2].data.push(this.dadosCalculo.result.dados.lmax);
-      }
-    }
-  }
+	ionViewWillEnter() {
+		if (this.platform.is("mobile")) {
+			this.screenOrientation.unlock();
+		}
+
+		if (this.serviceDados.getDados()) {
+			localStorage.setItem('Dados', JSON.stringify(this.serviceDados.getDados()));
+		}
+
+		let dados = localStorage.getItem('Dados');
+
+		if (dados) {
+			this.dadosCalculo = JSON.parse(dados);
+			console.log(this.dadosCalculo);
+
+			this.preencheCartaX();
+			this.preencheCartaAM();
+		}
+	}
+
+	preencheCartaX() {
+		this.labelsX = [];
+		this.cartaX[0].data = [];
+		this.cartaX[1].data = [];
+		this.cartaX[2].data = [];
+		this.cartaX[3].data = [];
+
+		if (this.dadosCalculo.obtidos?.length > 0) {
+			for (let index = 0; index < this.dadosCalculo.obtidos.length; index++) {
+				const element = this.dadosCalculo.obtidos[index];
+
+				this.labelsX.push(`${index + 1}`);
+
+				this.cartaX[0].data.push(element);
+				this.cartaX[1].data.push(this.dadosCalculo.resultados.dados.cartax.lic);
+				this.cartaX[2].data.push(this.dadosCalculo.resultados.dados.cartax.lm);
+				this.cartaX[3].data.push(this.dadosCalculo.resultados.dados.cartax.lsc);
+			}
+		}
+	}
+
+	preencheCartaAM() {
+		this.labelsAM = [];
+		this.cartaAM[0].data = [];
+		this.cartaAM[1].data = [];
+		this.cartaAM[2].data = [];
+		this.cartaAM[3].data = [];
+
+		if (this.dadosCalculo.resultados.dados.valoreVariancia.variancia?.length > 0) {
+			this.labelsAM.push('');
+			this.cartaAM[0].data.push(NaN);
+			this.cartaAM[1].data.push(this.dadosCalculo.resultados.dados.cartaam.lic);
+			this.cartaAM[2].data.push(this.dadosCalculo.resultados.dados.cartaam.lm);
+			this.cartaAM[3].data.push(this.dadosCalculo.resultados.dados.cartaam.lsc);
+
+			for (let index = 0; index < this.dadosCalculo.resultados.dados.valoreVariancia.variancia.length; index++) {
+				const element = this.dadosCalculo.resultados.dados.valoreVariancia.variancia[index];
+
+				this.labelsAM.push('');
+
+				this.cartaAM[0].data.push(element);
+				this.cartaAM[1].data.push(this.dadosCalculo.resultados.dados.cartaam.lic);
+				this.cartaAM[2].data.push(this.dadosCalculo.resultados.dados.cartaam.lm);
+				this.cartaAM[3].data.push(this.dadosCalculo.resultados.dados.cartaam.lsc);
+			}
+		}
+	}
 }
